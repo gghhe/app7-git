acce141ea4963ed228e23dac5ac5aca56b49d0d3
gghhe <38216299+gghhe@users.noreply.github.com>
2018-05-23 08:50:23 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){ //escuchador de eventos
 		$('#izquierda').on("swipeleft",function(){ //deslizar a la izquierda
-			navigator.notification.alert("Deslizó hcaia la izquierda",function(){"aplicacion 7", "ok"}); //aparece mensaje alert
+			navigator.notification.alert("Deslizó hacia la izquierda",function(){"aplicacion 7", "ok"}); //aparece mensaje alert
 		}); //cierre swipe izquierda
 		$('#derecha').on("swiperight",function(){ //deslizar derecha
 			navigator.notification.confirm("¿qué quieres hacer?",function(op){ //aparece mensaje para confirmar
